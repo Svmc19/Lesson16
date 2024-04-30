d72fa2b6a750c9c9b95149d568c176a4a67e46fd
Maftuna <svmca19@gmail.com>
2024-04-30 22:51:48 +0500

    tasks

---
+++ main.py
@@ -0,0 +1,30 @@
+#Task1
+ # son = int(input("Sonni kiriting:"))
+#
+# def max(lst,son):
+#     if son in lst:
+#         index = lst.index(son)
+#         return True
+#     return False
+#
+# print(max([1,3,5,12,26,95,100],son))
+
+#Task2
+#
+# def num(lst):
+#     for x in lst:
+#         if x + x <= 100:
+#             return True
+#
+#         return False
+#
+# print(num([88,87]))
+
+#Task3
+#
+# def add(lst):
+#
+#     big_numbers = lst[2] + lst[8] +lst[7] +lst [3] + lst[5]
+#     return big_numbers
+#
+# print(add([13,5,98,32,3,24,8,55,76,0]))
